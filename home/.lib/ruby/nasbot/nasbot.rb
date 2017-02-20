076db1caf6d5d0cf2b312a5daacceaa57649ae5b
Tyler Langlois <tjl@byu.net>
2017-02-20 00:21:11 -0500

    nasbot: add download command

--- home/.lib/ruby/nasbot/nasbot.rb
+++ home/.lib/ruby/nasbot/nasbot.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require 'English'
 require 'rubygems'
 require 'slack-ruby-bot'
 require 'mechanize'
@@ -10,14 +11,36 @@ module NASBot
   class App < SlackRubyBot::App
   end
 
+  # Auto-fetching video tracks
+  class YouTubeDL < SlackRubyBot::Commands::Base
+    match(/^(?:download) (?<url>[^\s]+)/i) do |client, data, match|
+      stdout = `
+        cd $HOME/memecd
+        youtube-dl --extract-audio
+                   --prefer-ffmpeg
+                   --audio-format mp3
+                   --audio-quality 3
+                   "#{match[:url]}"
+        cd -`
+
+      if $CHILD_STATUS.success?
+        m = stdout.match(/.ffmpeg. Destination: (?<song>[^\n]+)/)
+        message = "Fetched song successfully: #{m[:song]}"
+      else
+        message = "Error: #{stdout}"
+      end
+
+      client.say(channel: data.channel, text: message)
+    end
+  end
+
+  # Some web site automation
   class IPTorrents < SlackRubyBot::Commands::Base
-    match /(?:iptorrents|ipt) .*(?:balance|points|bonus)/i do |client, data, _match|
+    match(/(?:iptorrents|ipt) .*(?:balance|points|bonus)/i) do |client, data, _match|
       password = ENV['IPTORRENTS_PASSWORD']
       username = ENV['IPTORRENTS_USERNAME']
 
-      unless password
-        client.say(channel: data.channel, text: "Whoops, missing a password for #{username}.")
-      else
+      if password
         agent = Mechanize.new
         agent.get('https://iptorrents.eu/login.php') do |login_page|
           login_form = login_page.form
@@ -34,6 +57,11 @@ module NASBot
           logout = authenticated.form_with(:action => '/lout.php')
           agent.submit(logout)
         end
+      else
+        client.say(
+          channel: data.channel,
+          text: "Whoops, missing a password for #{username}."
+        )
       end
     end
   end
