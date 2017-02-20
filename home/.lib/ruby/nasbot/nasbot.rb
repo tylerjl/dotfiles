5692567788592f681ea49dadec8a5e0135b85ea7
Tyler Langlois <tjl@byu.net>
2017-02-19 23:51:10 -0500

    nasbot: tweak indentation style

--- home/.lib/ruby/nasbot/nasbot.rb
+++ home/.lib/ruby/nasbot/nasbot.rb
@@ -7,36 +7,36 @@ require 'mechanize'
 SlackRubyBot::Client.logger.level = Logger::WARN
 
 module NASBot
-    class App < SlackRubyBot::App
-    end
+  class App < SlackRubyBot::App
+  end
 
-    class IPTorrents < SlackRubyBot::Commands::Base
-        match /(?:iptorrents|ipt) .*(?:balance|points|bonus)/i do |client, data, _match|
-            password = ENV['IPTORRENTS_PASSWORD']
-            username = ENV['IPTORRENTS_USERNAME']
+  class IPTorrents < SlackRubyBot::Commands::Base
+    match /(?:iptorrents|ipt) .*(?:balance|points|bonus)/i do |client, data, _match|
+      password = ENV['IPTORRENTS_PASSWORD']
+      username = ENV['IPTORRENTS_USERNAME']
 
-            unless password
-                client.say(channel: data.channel, text: "Whoops, missing a password for #{username}.")
-            else
-                agent = Mechanize.new
-                agent.get('https://iptorrents.eu/login.php') do |login_page|
-                    login_form = login_page.form
-                    login_form.username = username
-                    login_form.password = password
+      unless password
+        client.say(channel: data.channel, text: "Whoops, missing a password for #{username}.")
+      else
+        agent = Mechanize.new
+        agent.get('https://iptorrents.eu/login.php') do |login_page|
+          login_form = login_page.form
+          login_form.username = username
+          login_form.password = password
 
-                    authenticated = agent.submit(login_form, login_form.buttons.first)
+          authenticated = agent.submit(login_form, login_form.buttons.first)
 
-                    bonus_page = authenticated.link_with(:href => '/mybonus.php').click
+          bonus_page = authenticated.link_with(:href => '/mybonus.php').click
 
-                    bonus_points = bonus_page.search("//font[@id='totalBP']").text.to_i
-                    client.say(channel: data.channel, text: "#{username} has #{bonus_points} bonus points.")
-                    puts 'Logging out...'
-                    logout = authenticated.form_with(:action => '/lout.php')
-                    agent.submit(logout)
-                end
-            end
+          bonus_points = bonus_page.search("//font[@id='totalBP']").text.to_i
+          client.say(channel: data.channel, text: "#{username} has #{bonus_points} bonus points.")
+          puts 'Logging out...'
+          logout = authenticated.form_with(:action => '/lout.php')
+          agent.submit(logout)
         end
+      end
     end
+  end
 end
 
 NASBot::App.instance.run
